0d83124aa525411343cf63c7cce516262a06ab4f
Alexis Peñuñuri <alxpesa@gmail.com>
2024-05-08 07:18:50 -0700

    Added basic functions to the backend API

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
 # _FSDI 108: Introductory React_
 Online store project made with React.
 
-## Install create-react-app locally
+## Frontend
+
+**Install create-react-app locally** \
 `` npm install -g create-react-app ``
 
-## Create a new React app
+**Create a new React app** \
 `` npx create-react-app app ``
 
-## Install react-router-dom
+**Install react-router-dom** \
 `` npx install -s react-router-dom ``
 
-## How to start the application
-`` npm install ``
+**Start the frontend** \
+`` npm install `` \
 `` npm start ``
+
+## Backend
+
+**Enable virtual environment** \
+`` venv\Scripts\activate ``
+
+**Install Flask, Mongoose and PyMongo** \
+`` py -m pip install flask mongoose pymongo "pymongo[srv]" ``
+
+**Install Certifi** \
+`` py -m pip install certifi ``
+
+**Start the backend** \
+`` flask --app server --debug run ``
---
+++ server/server.py
@@ -0,0 +1,162 @@
+from flask import Flask, request, abort # type: ignore
+import json
+from config import db
+from bson import ObjectId # type: ignore
+
+app = Flask(__name__)
+products = []
+
+@app.get("/")
+def home():
+  return "Hello from Flask"
+
+def fix_id(record):
+  record["_id"] = str(record["_id"])
+  return record
+
+@app.get("/api/products")
+def get_products():
+  catalog = []
+  cursor = db.products.find({})
+
+  for record in cursor:
+    fixed_record = fix_id(record)
+    catalog.append(fixed_record)
+
+  return json.dumps(catalog)
+
+@app.post("/api/products")
+def add_product():
+  prod = request.get_json()
+  db.products.insert_one(prod)
+
+  return json.dumps(fix_id(prod))
+
+""" @app.get("/api/categories")
+def get_categories():
+  cats = []
+  category = ""
+  cursor = db.products.find({})
+  
+  for record in cursor:
+    if "category" in record:
+      category = record.get("category")
+      if category not in cats:
+        cats.append(category)
+
+  return json.dumps(cats) """
+
+@app.get("/api/categories")
+def get_categories():
+  cats = []
+  category = ""
+  cursor = db.products.find({})
+  
+  for record in cursor:
+    category = record["category"]
+    if category not in cats:
+      cats.append(category)
+
+  return json.dumps(cats)
+
+@app.get("/api/products/<product>")
+def get_product(product):
+  products = []
+  cursor = db.products.find({})
+
+  for record in cursor:
+    if record["title"] == product:
+      fixed_record = fix_id(record)
+      products.append(fixed_record)
+
+  if len(products) == 0:
+    return abort(404, "Product not found")
+
+  return json.dumps(products)
+
+@app.get("/api/products/<category>")
+def get_category(category):
+  catalog = []
+  cursor = db.products.find({})
+
+  for record in cursor:
+    if record["category"] == category:
+      fixed_record = fix_id(record)
+      catalog.append(fixed_record)
+
+  return json.dumps(catalog)
+
+@app.get("/api/reports/total")
+def get_total():
+  total = 0
+  cursor = db.products.find({})
+  
+  for record in cursor:
+    total += record["price"]
+
+  return json.dumps(total)
+
+@app.delete("/api/products/<id>")
+def delete_product(id):
+  if not ObjectId.is_valid(id):
+    return abort(400, "Invalid ID")
+
+  db_id = ObjectId(id)
+  result = db.products.delete_one({"_id": db_id})
+
+  if result.deleted_count == 0:
+    return abort(404, "Product not found")
+  
+  return json.dumps({"deleted": True})
+
+@app.get("/api/coupons")
+def get_coupons():
+  coupons = []
+  cursor = db.coupons.find({})
+  
+  for record in cursor:
+    fixed_record = fix_id(record)
+    coupons.append(fixed_record)
+
+  return json.dumps(coupons)
+
+@app.post("/api/coupons")
+def add_coupon():
+  coupon = request.get_json()
+  db.coupons.insert_one(coupon)
+
+  return json.dumps(fix_id(coupon))
+
+""" @app.get("/api/coupons/<code>")
+def get_coupon(code):
+  cursor = db.coupons.find({})
+
+  for record in cursor:
+    if record["code"] == code:
+      return json.dumps(fix_id(record))
+    else:
+      return abort(404, "Coupon not found") """
+
+@app.get("/api/coupons/<code>")
+def get_coupon(code):
+  coupon = db.coupons.find_one({"code": code})
+
+  if not coupon:
+    return abort(404, "Coupon not found")
+
+  return json.dumps(fix_id(coupon))
+
+@app.delete("/api/coupons/<id>")
+def delete_coupon(id):
+  if not ObjectId.is_valid(id):
+    return abort(400, "Invalid ID")
+
+  db_id = ObjectId(id)
+  result = db.coupons.delete_one({"_id": db_id})
+
+  if result.deleted_count == 0:
+    return abort(404, "Coupon not found")
+  
+  return json.dumps({"deleted": True})
+
+# app.run(debug = True)
